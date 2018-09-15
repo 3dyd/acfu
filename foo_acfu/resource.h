c5904f8da8b115c11a69afe7d7df3c1b3669b024
Mikalai Ananenka <git@3dyd.com>
2018-09-15 17:44:54 +0300

    sort content of embedded sources list

--- foo_acfu/resource.h
+++ foo_acfu/resource.h
@@ -34,6 +34,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40007
 #define _APS_NEXT_CONTROL_VALUE         1008
